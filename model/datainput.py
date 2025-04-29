bb18a7434ec91d24e46a24b52d2db42e476264b2
RedMarcher <jxuwei@ucdavis.edu>
2025-04-29 23:56:05 +0000

    Fixed data ingester

--- model/datainput.py
+++ model/datainput.py
@@ -1,22 +1,33 @@
 import pandas as pd
+import os
 
-def load_data_from_xlsx(file_path, target, features):
-    """
-    Loads data from an Excel file and prepares it for machine learning.
+DATA_FILE_PATH = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'data', 'heart_og.csv')
 
-    Args:
-        file_path (str): Path to the Excel file.
-        target_column (str): Name of the target column for machine learning.
+"""
+Loads data from an Excel file and prepares it for machine learning.
 
-    Returns:
-        X (pd.DataFrame): Features for training.
-        y (pd.Series): Target variable.
-    """
-    # Load the Excel file into a DataFrame
-    data = pd.read_excel(file_path)
+Args:
+    file_path (str): Path to the Excel file.
+    target_column (str): Name of the target column for machine learning.
 
-    # Separate features (X) and target (y)
-    X = data[features]
-    y = data[target]
+Returns:
+    X (pd.DataFrame): Features for training.
+    y (pd.Series): Target variable.
+"""
 
-    return X, y
+def load_data_from_xlsx(target, features, file_path=DATA_FILE_PATH):
+    try:
+        # Load the Excel file into a DataFrame
+        data = pd.read_csv(file_path)
+
+        # Separate features (X) and target (y)
+        X = data[features]
+        y = data[target]
+
+        return X, y
+    except FileNotFoundError:
+        print(f"Error: File not found at {file_path}")
+        return None, None
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None, None
